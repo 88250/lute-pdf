48e4ade22e192ad6260dc27671d566979e879bf2
Liang Ding <d@b3log.org>
2020-02-27 22:27:01 +0800

    :tada: 初始提交

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/88250/lute-pdf
+
+go 1.14
+
+require (
+	github.com/chromedp/cdproto v0.0.0-20200209033844-7e00b02ea7d2 // indirect
+	github.com/chromedp/chromedp v0.5.3 // indirect
+	github.com/mailru/easyjson v0.7.1 // indirect
+	github.com/phpdave11/gofpdi v1.0.11 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/signintech/gopdf v0.9.5
+	golang.org/x/sys v0.0.0-20200223170610-d5e6a3e2c0ae // indirect
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log"
+	"math"
+
+	"github.com/signintech/gopdf"
+)
+
+func main() {
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4}) // 595.28, 841.89 = A4
+	pdf.AddPage()
+
+	var err error
+	err = pdf.AddTTFFont("msyh", "msyh.ttf")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = pdf.AddTTFFontWithOption("msyhb", "msyhb.ttf", gopdf.TtfOption{Style: gopdf.Bold})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	const factor = 0.8
+	fontSize := 16 * factor
+	x := 16.0 * factor
+	y := 24.0 * factor
+
+	const lineHeight = 24.0 * factor
+	const heading2Size = 24 * factor
+
+	if err = pdf.SetFontWithStyle("msyhb", gopdf.Bold, int(math.Round(heading2Size))); nil != err {
+		log.Fatal(err)
+	}
+	pdf.SetX(x)
+	pdf.SetY(y)
+	_ = pdf.Cell(nil, "Guide")
+	y += heading2Size + lineHeight
+	pdf.SetX(x)
+	pdf.SetY(y)
+
+	pdf.SetFontWithStyle("msyh", gopdf.Regular, int(fontSize))
+	pdf.SetFont("msyh", "", int(fontSize))
+
+	rect := &gopdf.Rect{W: gopdf.PageSizeA4.W - x*2, H: gopdf.PageSizeA4.H}
+	pdf.MultiCell(rect, "这是一篇讲解如何正确使用 Markdown 的排版示例，学会这个很有必要，能让你的文章有更佳清晰的排版。")
+
+	pdf.WritePdf("hello.pdf")
+
+}
